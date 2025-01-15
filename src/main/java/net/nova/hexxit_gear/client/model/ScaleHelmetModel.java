b90f507eb993c898aaf21ab52b8b7e12f9826356
Starexify <nightbear843@yahoo.com>
2025-01-16 00:13:17 +0100

    Fixed Scale Helmet to work

---
+++ src/main/java/net/nova/hexxit_gear/client/model/ScaleHelmetModel.java
@@ -0,0 +1,38 @@
+package net.nova.hexxit_gear.client.model;
+
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.nova.hexxit_gear.HexxitGearR;
+
+public class ScaleHelmetModel extends BaseHelmetModel {
+    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(HexxitGearR.rl("scale_helmet"), "main");
+    public static final ResourceLocation TEXTURE = HexxitGearR.rl("textures/entity/equipment/helmet/scale.png");
+
+    public ScaleHelmetModel(ModelPart root) {
+        super(root);
+    }
+
+    public static LayerDefinition createLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        PartDefinition helmet = partdefinition.addOrReplaceChild("helmet", CubeListBuilder.create().texOffs(0, 0).addBox(-5.0F, -9.0F, -5.0F, 10.0F, 9.0F, 10.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 19).addBox(-4.0F, -8.0F, -4.75F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
+                .texOffs(41, 0).addBox(-6.0F, -7.0F, -2.0F, 1.0F, 5.0F, 3.0F, new CubeDeformation(0.0F))
+                .texOffs(50, 2).addBox(-7.0F, -8.0F, 0.0F, 1.0F, 4.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(57, 4).addBox(-8.0F, -8.0F, 1.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(42, 9).addBox(-9.0F, -8.0F, 2.0F, 2.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(42, 9).addBox(-9.0F, -6.0F, 2.0F, 2.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(41, 0).addBox(5.0F, -7.0F, -2.0F, 1.0F, 5.0F, 3.0F, new CubeDeformation(0.0F))
+                .texOffs(50, 2).addBox(6.0F, -8.0F, 0.0F, 1.0F, 4.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(57, 4).addBox(7.0F, -8.0F, 1.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(42, 9).addBox(7.0F, -8.0F, 2.0F, 2.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(42, 9).addBox(7.0F, -6.0F, 2.0F, 2.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+        return LayerDefinition.create(meshdefinition, 64, 35);
+    }
+}
--- src/main/java/net/nova/hexxit_gear/event/ClientEvents.java
+++ src/main/java/net/nova/hexxit_gear/event/ClientEvents.java
@@ -1,14 +1,23 @@
 package net.nova.hexxit_gear.event;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.Model;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.resources.model.EquipmentClientInfo;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
+import net.nova.hexxit_gear.client.model.ScaleHelmetModel;
 import net.nova.hexxit_gear.init.HGBlocks;
+import net.nova.hexxit_gear.init.HGItems;
 
 import static net.nova.hexxit_gear.HexxitGearR.MODID;
 
@@ -23,17 +32,35 @@ public class ClientEvents {
 
     @SubscribeEvent
     public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
-/*        event.registerLayerDefinition(ScaleHelmetModel.LAYER_LOCATION, ScaleHelmetModel::createLayer);
-        event.registerLayerDefinition(TribalSkullModel.LAYER_LOCATION, TribalSkullModel::createLayer);
-        event.registerLayerDefinition(ThiefHoodModel.LAYER_LOCATION, ThiefHoodModel::createLayer);
-        event.registerLayerDefinition(SageHoodModel.LAYER_LOCATION, SageHoodModel::createLayer);*/
+        event.registerLayerDefinition(ScaleHelmetModel.LAYER_LOCATION, ScaleHelmetModel::createLayer);
     }
 
     @SubscribeEvent
     public static void onRegisterClientExtensions(RegisterClientExtensionsEvent event) {
-/*        event.registerItem(ISTERProvider.scaleHelmet(), HGItems.SCALE_HELMET);
-        event.registerItem(ISTERProvider.tribalSkull(), HGItems.TRIBAL_SKULL);
-        event.registerItem(ISTERProvider.thiefHood(), HGItems.THIEF_HOOD);
-        event.registerItem(ISTERProvider.sageHood(), HGItems.SAGE_HOOD);*/
+        event.registerItem(scaleHelmet(), HGItems.SCALE_HELMET);
     }
+
+    public static IClientItemExtensions scaleHelmet() {
+        return new IClientItemExtensions() {
+            public static ScaleHelmetModel scaleHelmet;
+
+            @Override
+            public Model getHumanoidArmorModel(ItemStack itemStack, EquipmentClientInfo.LayerType layerType, Model original) {
+                if (original instanceof HumanoidModel<?> humanoidModel) {
+                    if (scaleHelmet == null) {
+                        scaleHelmet = new ScaleHelmetModel(Minecraft.getInstance().getEntityModels().bakeLayer(ScaleHelmetModel.LAYER_LOCATION));
+                        return scaleHelmet;
+                    }
+                    scaleHelmet.helmet.copyFrom(humanoidModel.getHead());
+                }
+                return scaleHelmet;
+            }
+
+            @Override
+            public ResourceLocation getArmorTexture(ItemStack stack, EquipmentClientInfo.LayerType type, EquipmentClientInfo.Layer layer, ResourceLocation _default) {
+                return ScaleHelmetModel.TEXTURE;
+            }
+        };
+    }
+
 }
