6e0452c2648321b41553c7ab4d1a138a837b1032
Starexify <nightbear843@yahoo.com>
2025-01-15 20:40:20 +0100

    Stuff for Armor Repair and Blocks (also fixed the Hexbiscus translation)

--- src/main/java/net/nova/hexxit_gear/data/tags/HGItemTagsProvider.java
+++ src/main/java/net/nova/hexxit_gear/data/tags/HGItemTagsProvider.java
@@ -5,6 +5,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.tags.ItemTags;
 import net.nova.hexxit_gear.init.HGItems;
+import net.nova.hexxit_gear.init.HGTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -21,5 +22,9 @@ public class HGItemTagsProvider extends ItemTagsProvider {
         tag(ItemTags.CHEST_ARMOR).add(HGItems.SCALE_CHESTGUARD.get(), HGItems.TRIBAL_TUNIC.get(), HGItems.THIEF_TUNIC.get(), HGItems.SAGE_ROBE.get());
         tag(ItemTags.LEG_ARMOR).add(HGItems.SCALE_LEGGINGS.get(), HGItems.TRIBAL_LEGGINGS.get(), HGItems.THIEF_TROUSERS.get(), HGItems.SAGE_PANTS.get());
         tag(ItemTags.FOOT_ARMOR).add(HGItems.SCALE_BOOTS.get(), HGItems.TRIBAL_WARBOOTS.get(), HGItems.THIEF_TURNSHOES.get(), HGItems.SAGE_WALKERS.get());
+        tag(HGTags.ItemTag.REPAIRS_SAGE_ARMOR).add(HGItems.HEXICAL_DIAMOND.get());
+        tag(HGTags.ItemTag.REPAIRS_TRIBAL_ARMOR).add(HGItems.HEXICAL_DIAMOND.get());
+        tag(HGTags.ItemTag.REPAIRS_THIEF_ARMOR).add(HGItems.HEXICAL_DIAMOND.get());
+        tag(HGTags.ItemTag.REPAIRS_SAGE_ARMOR).add(HGItems.HEXICAL_DIAMOND.get());
     }
 }
