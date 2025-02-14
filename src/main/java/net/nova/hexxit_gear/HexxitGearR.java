f29518112127afe527d989c50967d514486fd3b6
Starexify <nightbear843@yahoo.com>
2025-02-14 21:26:13 +0100

    Made Hexbiscus Emissive and rarer

--- src/main/java/net/nova/hexxit_gear/HexxitGearR.java
+++ src/main/java/net/nova/hexxit_gear/HexxitGearR.java
@@ -10,15 +10,12 @@ import net.neoforged.fml.common.Mod;
 import net.nova.hexxit_gear.init.CreativeTab;
 import net.nova.hexxit_gear.init.HGBlocks;
 import net.nova.hexxit_gear.init.HGItems;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static net.nova.hexxit_gear.HexxitGearR.MODID;
 
 @Mod(MODID)
 public class HexxitGearR {
     public static final String MODID = "hexxit_gear";
-    public static final Logger logger = LoggerFactory.getLogger(HexxitGearR.class);
 
     public HexxitGearR(IEventBus bus) {
         CreativeTab.CREATIVE_TAB.register(bus);
