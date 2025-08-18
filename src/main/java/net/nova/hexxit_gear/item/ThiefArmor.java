8c3802a9daed28f08ffa4c380731f09c22786d22
ImVeryBad <nightbear843@yahoo.com>
2025-08-18 22:51:17 +0200

    Modified Full Thief Set to work similarly to the older version by changing the attributes and also added step-up ability.

--- src/main/java/net/nova/hexxit_gear/item/ThiefArmor.java
+++ src/main/java/net/nova/hexxit_gear/item/ThiefArmor.java
@@ -1,16 +1,24 @@
 package net.nova.hexxit_gear.item;
 
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.*;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.nova.hexxit_gear.HexxitGearR;
 import org.jetbrains.annotations.Nullable;
 
+import static net.nova.hexxit_gear.HexxitGearR.MODID;
+
 public class ThiefArmor extends Item {
+    public ResourceLocation SPEED_MODIFIER_ID = ResourceLocation.fromNamespaceAndPath(MODID, "thief_speed_modifier");
+    public ResourceLocation STEP_MODIFIER_ID = ResourceLocation.fromNamespaceAndPath(MODID, "thief_step_modifier");
+
     public ThiefArmor(Properties properties) {
         super(properties);
     }
@@ -24,6 +32,8 @@ public class ThiefArmor extends Item {
             for (EquipmentSlot slot : new EquipmentSlot[]{EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET})
                 if (!(livingEntity.getItemBySlot(slot).getItem() instanceof ThiefArmor)) isWearingFullSet = false;
 
+            applyOrRemoveModifier(livingEntity, Attributes.MOVEMENT_SPEED, SPEED_MODIFIER_ID, 0.15F, isWearingFullSet);
+            applyOrRemoveModifier(livingEntity, Attributes.STEP_HEIGHT, STEP_MODIFIER_ID, 0.4F, isWearingFullSet);
             if (isWearingFullSet) addFullSetEffects(livingEntity);
         }
     }
@@ -31,6 +41,16 @@ public class ThiefArmor extends Item {
     public void addFullSetEffects(LivingEntity livingEntity) {
         HexxitGearR.addEffect(livingEntity, MobEffects.NIGHT_VISION, 300, 0);
         HexxitGearR.addEffect(livingEntity, MobEffects.STRENGTH, 1, 0);
-        HexxitGearR.addEffect(livingEntity, MobEffects.SPEED, 1, 0);
     }
+
+    public void applyOrRemoveModifier(LivingEntity entity, Holder<Attribute> attribute, ResourceLocation modifierId, float value, boolean shouldApply) {
+        AttributeInstance attributeInstance = entity.getAttributes().getInstance(attribute);
+        if (attributeInstance == null) return;
+
+        boolean hasModifier = attributeInstance.hasModifier(modifierId);
+
+        if (shouldApply && !hasModifier) attributeInstance.addOrUpdateTransientModifier(new AttributeModifier(modifierId, value, AttributeModifier.Operation.ADD_VALUE));
+        else if (!shouldApply && hasModifier) attributeInstance.removeModifier(modifierId);
+    }
+
 }
